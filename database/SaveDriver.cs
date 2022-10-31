d6a2660f2e0c45311f5173361b3f60ab5d9e7ca7
Lacy <44073703+lacyrich@users.noreply.github.com>
2022-10-31 14:48:33 -0500

    hi

--- database/SaveDriver.cs
+++ database/SaveDriver.cs
@@ -25,11 +25,11 @@ namespace mis321pa3.database
             using var con = new MySqlConnection(cs);
             con.Open();
 
-            string stm = @"INSERT INTO drivers(driverName, rating, dateHired, isDeleted) VALUES(@name, @rating, @dateHired, @isDeleted)";
+            string stm = @"INSERT INTO drivers(driverName, rating, dateHired, isDeleted) VALUES(@driverName, @rating, @dateHired, @isDeleted)";
 
             using var cmd = new MySqlCommand(stm, con);
 
-            cmd.Parameters.AddWithValue("@name", myDriver.DriverName);
+            cmd.Parameters.AddWithValue("@driverName", myDriver.DriverName);
             cmd.Parameters.AddWithValue("@rating", myDriver.Rating);
             cmd.Parameters.AddWithValue("@dateHired", myDriver.DateHired);
             cmd.Parameters.AddWithValue("@isDeleted", myDriver.Deleted);
@@ -39,9 +39,29 @@ namespace mis321pa3.database
             cmd.ExecuteNonQuery();
         }
 
+        public void ShowDrivers(Driver myDriver){
+            ConnectionString myConnection = new ConnectionString();
+            string cs = myConnection.cs;
+            using var con = new MySqlConnection(cs);
+            con.Open();
+
+            string stm = @"SELECT driverName from drivers WHERE isDeleted = 0  VALUES(@driverName, @rating";
+
+            using var cmd = new MySqlCommand(stm, con);
+
+            cmd.Parameters.AddWithValue("@driverName", myDriver.DriverName);
+            cmd.Parameters.AddWithValue("@rating", myDriver.Rating);
+            
+
+            cmd.Prepare();
+            
+            cmd.ExecuteNonQuery();
+        }
         void ISaveDriver.SaveDriver(Driver myDriver)
         {
             throw new System.NotImplementedException();
         }
+
+        
     }
 }
